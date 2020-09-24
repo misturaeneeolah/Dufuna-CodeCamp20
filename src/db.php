45b9dd3f1bcbcb2ded49e13639652564901ee3b0
Adaa <adaamgbede@gmail.com>
2020-09-24 20:16:49 +0100

    first commit

--- src/db.php
+++ src/db.php
@@ -1,14 +1,5 @@
 <?php
 
-//$repository = Dotenv\Repository\RepositoryBuilder::createWithNoAdapters()
-//    ->addAdapter(Dotenv\Repository\Adapter\EnvConstAdapter::class)
-//    ->addWriter(Dotenv\Repository\Adapter\PutenvAdapter::class)
-//    ->immutable()
-//    ->make();
-//
-//$dotenv = Dotenv\Dotenv::create($repository, __DIR__ .'\env');
-//$dotenv->load();
-
 class DB {
     protected $db_hostname;
     protected $db_username;
